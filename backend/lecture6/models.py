fee5fcaa0f59a5af5f1ca004cd32a6b35b77580f
vutho <vutho2803@gmail.com>
2024-12-13 17:30:06 +0200

    Resolved conflict by using remote db.sqlite3

--- backend/lecture6/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Card(models.Model):
-    color = models.CharField(max_length=10)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.color
